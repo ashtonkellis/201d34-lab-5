0ee731b79816de83b043814052096320c34ea171
Ashton K Ellis <ashtonkellis@gmail.com>
2018-04-06 16:22:52 -0700

    problem 5 complete

--- app.js
+++ app.js
@@ -89,7 +89,7 @@ function sumArray(sumArr) { //eslint-disable-line
 
 // Here is the test for sumArray(); uncomment it to run it
 
-testSumArray(testArray);
+// testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -105,11 +105,16 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
-
+  var productNumeric = 1;
+  for (var i = 0; i < multArr.length; i++) {
+    productNumeric = multiply(productNumeric, multArr[i])[0];
+  }    
+  var productString = 'The numbers ' + multArr.join(',') + ' have a product of ' + productNumeric + '.';
+  return [productNumeric, productString]; 
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
